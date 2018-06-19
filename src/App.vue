ff2bb7397c748b495d9e15e9a2e6dcb057f9d95a
xhy <695487524@qq.com>
2018-06-19 10:37:44 +0800

    新建AddBlog.vue,并写了一些数据绑定

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <addBlog></addBlog>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import AddBlog from './components/AddBlog'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddBlog
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/AddBlog.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <form>
+      <ul>
+        <li>
+          <label>标题:</label>
+          <input type="text" v-model="blog.title"/>
+        </li>
+        <li>
+          <label>内容:</label>
+          <textarea v-model="blog.content"></textarea>
+        </li>
+        <li>
+          <label>分类:</label>
+          <input type="checkbox" value="vue.js" v-model="blog.checkedNames">
+          <label for="jack">vue.js</label>
+          <input type="checkbox" value="react.js" v-model="blog.checkedNames">
+          <label for="john">react.js</label>
+          <input type="checkbox" value="angular.js" v-model="blog.checkedNames">
+          <label for="mike">angle.js</label>
+        </li>
+        <li>
+          <label>作者:</label>
+          <select v-model="blog.author">
+            <option v-for="author in authors">{{author}}</option>
+          </select>
+        </li>
+      </ul>
+      <button @click.prevent="postData">添加</button>
+    </form>
+
+    <hr>
+    <div id="preview">
+      <p>博客标题:{{blog.title}}</p>
+      <p>博客内容:{{blog.content}}</p>
+      <p>分类:{{blog.checkedNames}}</p>
+      <p>作者:{{blog.author}}</p>0
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'addBlog',
+    data () {
+      return {
+        msg: '添加博客',
+        blog:{
+          title:'',
+          content:'',
+          checkedNames:[],
+          author:''
+        },
+        authors:['jolin','jaychou','ella','selina']
+      }
+    },
+    methods:{
+      postData(){
+        console.log('djskjdks')
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
