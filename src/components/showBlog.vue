3aa366f2a8204f434320502454b3e9cafe1f09be
xhy <695487524@qq.com>
2018-06-20 16:59:16 +0800

    add filters and search part

--- src/components/showBlog.vue
+++ src/components/showBlog.vue
@@ -1,10 +1,11 @@
 <template>
-  <div v-theme:lisjs="'ran'" class="showBlog">
-    <h1 >我是博客总览页面</h1>
-    <div v-for="blog in blogs" class="blogInfo">
-        <h4 v-color>{{blog.title}}</h4>
+  <div v-theme="'ran'" class="showBlog">
+    <h1>我是博客总览页面</h1>
+    <div class="search"><input v-model="search" type="text" placeholder="搜索"></div>
+    <div v-for="blog in filterBlogs" class="blogInfo">
+        <h4 v-rainbow>{{blog.title | to-upCass}}</h4>
         <article>
-          {{blog.body}}
+          {{blog.body | snippet}}
         </article>
     </div>
   </div>
@@ -16,7 +17,8 @@
     name: 'showBlog',
     data(){
       return{
-        blogs:[]
+        blogs:[],
+        search:''
       }
     },
     created(){
@@ -24,12 +26,38 @@
         console.log(data)
         this.blogs = data.data.slice(0,6)
       })
-
+    },
+    computed:{
+      filterBlogs(){
+        return this.blogs.filter((blog) =>{
+          return blog.title.match(this.search)
+        })
+      }
+    },
+    filters:{
+//      "to-upCass":function(value){
+//        return value.toUpperCase();
+//      }
+      toUpCass(value){
+        return value.toUpperCase();
+      },
+      snippet(value){
+        return value.slice(0,10);
+      }
+    },
+    directives:{
+      'rainbow':{
+        bind(el,bind,vnode){
+          el.style.color = '#'+ Math.random().toString(16).slice(2,8);//彩虹色
+        }
+      }
     }
+
   }
 </script>
 
 <style>
+h1,.search{text-align: center}
 .blogInfo{ background-color: #eee;width: 600px;padding:20px;margin: 0 auto 20px auto}
-  .blogInfo h4{margin: 0;}
+.blogInfo h4{margin: 0;}
 </style>
--- src/main.js
+++ src/main.js
@@ -9,25 +9,35 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
-//注册一个全局自定义指令
-Vue.directive('color',{
-  bind(el){
-    el.style.color = 'red'
-  }
-})
+// 注册一个全局自定义指令
+// Vue.directive('rainbow',{
+//   bind(el){
+//     el.style.color = 'red'
+//   }
+// })
 
 Vue.directive('theme',{
   bind(el,binding){
     if(binding.value == 'wide'){
       el.style.color = 'green'
     }else if(binding.value == 'ran'){
-      el.style.color = 'blue'
+      el.style.color = '#313131'
     }
     if(binding.arg == 'lisjs'){
       el.style.background = 'green'
     }
   }
 })
+
+// 注册一个全局的过滤器
+// Vue.filter('to-upCass',function(value){
+//   return value.toUpperCase();
+// })
+// Vue.filter('snippet',function(value){
+//   return value.slice(0,40);
+// })
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
